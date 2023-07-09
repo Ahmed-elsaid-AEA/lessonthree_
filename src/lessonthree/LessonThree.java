14b07d86bf135251c7abc408dab6fd3266714999
ahmed-abmouty <ahmed122727727@gmail.com>
2023-07-09 21:19:31 +0300

    uploading

---
+++ src/lessonthree/LessonThree.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lessonthree;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author A.E.A
+ */
+public class LessonThree {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        /*String name = "ahmed", subject = "ds";
+        int x = 1, y = 0;
+        System.out.println("x : " + x + " , y : " + y);
+//        final String appName = "managment App";
+//        System.out.println(appName);
+//        appName = "app2";
+//        System.out.println(appName);
+        //+ addition
+        // - subtraction
+        // * multiplication
+        // / division
+        // % modulus
+        int firstNumber = 11, secondNumber = 5;
+        System.out.println("addition " + (firstNumber + secondNumber));
+        System.out.println("multiplication " + (firstNumber * secondNumber));
+        System.out.println("subtraction " + (firstNumber - secondNumber));
+        System.out.println("division " + (firstNumber / secondNumber));
+        System.out.println("modulus " + (firstNumber % secondNumber));
+        System.out.println(10 % 11);
+         */
+//        int x = 20;
+//        x=x+1;
+//        x+=1;
+//        x++;//increment
+//        x--;//decrement
+//        x=x-1;
+//        x-=1;
+//        x=x+5;
+//        x+=5;
+//        x++;
+//        ++x;
+//        System.out.println(x);
+//        System.out.println(++x);
+//        System.out.println(x);
+//        int z = 11;
+//        int y = 10;
+//        int o = 1;
+//        System.out.println(z == y);
+//        System.out.println(z != y);
+//        System.out.println(z >= y);
+//        System.out.println(z > y || y < o);
+//        int y=5;
+//        int x=y;
+//        double xx=10.5;
+//        int t=(int)xx;
+//        System.out.println(xx);
+//        System.out.println(t);
+//        String name = "axxxahmeda ";
+//
+////        System.out.println(name.length());
+////        System.out.println(name.toUpperCase());
+////        String name2 = "AHMED ";
+////        name2 = name2.toLowerCase();
+////        System.out.println(name2);
+//        System.out.println(name.indexOf(97, 5));
+//        
+        String name;
+        int age;
+        System.out.println("Enter Your name : ");
+        Scanner input = new Scanner(System.in);
+        name = input.nextLine();
+        System.out.println("Enter Your age : ");
+        age=input.nextInt();
+        System.out.println("hello "+name+" , your age is " + age);
+//make a program that able to user to input name ,age ,salary ,gba 
+//        and in finall enter all data
+    }
+
+}
